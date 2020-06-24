962e73b588a292cb9cd5992789cec9e74fc836c0
ianhunterharold <ianhunterharold@gmail.com>
2020-06-24 17:25:00 -0600

    first working solution

---
+++ runningTotal.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    
+  let addedSum = []; 
+  let total = 0; 
+  
+  for (let i =0; i <nums.length; i++){
+      total += nums[i];
+      let addToAddedSum = addedSum.push(total);
+  }
+  
+  return addedSum;
+};
